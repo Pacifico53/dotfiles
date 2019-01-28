94f56aa11905650a04e9c7879082a76b555d3f2f
Bernardo Viseu <bernardo2viseu@gmail.com>
2019-01-28 22:45:45 +0000

    displays and bg stuff

---
+++ screenLayoutScripts/homeDual.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP1 --off --output eDP1 --mode 1920x1080 --pos 0x360 --rotate normal --output VIRTUAL1 --off
+feh --bg-scale ~/Pictures/Wallpapers/mountains.jpg ~/Pictures/Wallpapers/universeorange.jpg
