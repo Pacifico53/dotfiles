57e94b5c60ff8e46884f2682684dd5b0ac2fb30d
Bernardo Viseu <bernardo2viseu@gmail.com>
2019-01-29 16:36:01 +0000

    updates display scripts

--- screenLayoutScripts/normal.sh
+++ screenLayoutScripts/normal.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 feh --bg-scale ~/Pictures/Wallpapers/aphextwin.jpg
+pkill conky
+start_conky_grey
